8723bcde1bc73b753d061137e523b947e3493562
jasmyne nguyen <jasmynernguyen@gmail.com>
2026-07-13 14:01:22 -0700

    new page

---
+++ loghistory.js
@@ -0,0 +1,36 @@
+let previous_logs=[
+    {"date":"January 12", 
+     "previous_tasks":"x,b,c,d", 
+     "new_tasks": "q, w, e, r",
+    }
+
+]; 
+
+
+//define elements;
+const card_container=document.getElementById("card-container"); 
+const month_place=document.getElementById("month"); 
+const card_place=document.getElementById("day"); 
+const previous_tasks_container=document.getElementById("previous_tasks");
+const new_tasks_container=document.getElementById("new_tasks"); 
+
+
+//create new task UI
+function create_new_tasks_element(date, previous_tasks, new_tasks){
+    const temp_node=card_container.cloneNode(true); 
+    temp_node.querySelector(".span").textContent=date; 
+    
+    const previous_tasks_list=previous_tasks.split(",")
+    console.log(previous_tasks_list);
+
+
+    //Populate with data;
+    previous_tasks_list.forEach(previous_task => {
+        let temp_li=document.createElement("li"); 
+        temp_li.textContent=previous_task
+        previous_tasks_container.append(temp_li)
+        
+    });
+
+
+};
